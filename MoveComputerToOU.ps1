f663adc2bc0680b22a3dc45cd7571548304cb720
jlangdon11 <JordanLangdon11@gmail.com>
2025-10-08 11:48:24 -0400

    Update MoveComputerToOU.ps1

--- MoveComputerToOU.ps1
+++ MoveComputerToOU.ps1
@@ -2,4 +2,5 @@
 
 $Cred = Get-Credential
 
-Invoke-Command –ComputerName DC03 -Credential  $Cred –ScriptBlock{Import-Module activedirectory; Get-ADComputer mmaggard25 | Move-ADObject -TargetPath 'ou=Customer Service,ou=Domain Computers,dc=officelink,dc=local'; Get-ADComputer -Filter * -SearchBase "ou=Customer Service,ou=Domain Computers,dc=officelink,dc=local"}
+
+Invoke-Command –ComputerName DC03 -Credential  $Cred –ScriptBlock{et-ADComputer mmaggard25 | Move-ADObject -TargetPath 'ou=Customer Service,ou=Domain Computers,dc=officelink,dc=local'; Get-ADComputer -Filter * -SearchBase "ou=Customer Service,ou=Domain Computers,dc=officelink,dc=local"}
