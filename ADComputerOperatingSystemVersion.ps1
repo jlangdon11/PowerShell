05a544c07224d06a3dbc8142835c50d11681020d
jlangdon11 <JordanLangdon11@gmail.com>
2025-10-08 08:33:26 -0400

    Update ADComputerOperatingSystemVersion.ps1

--- ADComputerOperatingSystemVersion.ps1
+++ ADComputerOperatingSystemVersion.ps1
@@ -1,2 +1,3 @@
 $Cred = Get-Credential
-Invoke-Command -ComputerName DC03 -Credential $Cred -ScriptBlock {Get-ADComputer -Filter * -Property OperatingSystem,OperatingSystemVersion | Format-Table Name,OperatingSystem,OperatingSystemVersion}
+
+Invoke-Command -ComputerName  -Credential $Cred -ScriptBlock {Get-ADComputer -Filter * -Property OperatingSystem,OperatingSystemVersion | Format-Table Name,OperatingSystem,OperatingSystemVersion}
