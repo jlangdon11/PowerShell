eae8ee455bdadd0a4c910ca3366843d9b27c41c1
jlangdon11 <JordanLangdon11@gmail.com>
2025-10-08 11:27:49 -0400

    Update RunningProcesses.ps1

--- RunningProcesses.ps1
+++ RunningProcesses.ps1
@@ -9,11 +9,12 @@ This also shows the ID of the process as well as the Path it is located
 Get-Process | Format-Table -Wrap -AutoSize -Property Name,Id,Path
 
 
-# Sorts the process to chrome
+# Filters out process
 
-Get-Process -Name chrome | Format-Table -Wrap -AutoSize -Property Name,Id,Path
+Get-Process -Name *chrome* | Format-Table -Wrap -AutoSize -Property Name,Id,Path
 
 
 # Groups by company
+
 
 Get-Process | Format-Table -Wrap -AutoSize -Property Name,Id,Path -GroupBy Company
