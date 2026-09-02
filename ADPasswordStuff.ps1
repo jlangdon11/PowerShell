56ae90cd56153eda7c5ad824391e4b52f91ec7c2
jlangdon11 <JordanLangdon11@gmail.com>
2026-09-02 12:16:49 -0400

    Add files via upload

---
+++ ADPasswordStuff.ps1
@@ -0,0 +1,8 @@
+﻿# Returns the expiration date and time of a user's password
+
+Get-ADUser -identity langdonjm –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
+Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
+
+# Returns the date and time of password last set by a user
+
+Get-ADUser -identity bella -properties passwordlastset | ft Name, passwordlastset
