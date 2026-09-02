304988ce10a263dd7913af846354c22369cfb7d4
jlangdon11 <JordanLangdon11@gmail.com>
2026-09-02 12:17:19 -0400

    Add files via upload

---
+++ ReturnADUserDetails.ps1
@@ -0,0 +1,15 @@
+﻿# Returns user details like their Canonical Name and if they are locked out
+
+while ($true) {
+
+$Aduser = Read-Host "Enter the username"
+
+Get-ADUser -Filter "SamAccountName -like '*$Aduser*'" -Properties CanonicalName, LockedOut
+
+}
+
+<#if (LockedOut = true){
+    Unlock-ADAccount
+
+    Get-ADUser -Filter "SamAccountName -like '*$Aduser*'" -Properties CanonicalName, LockedOut
+}#>
