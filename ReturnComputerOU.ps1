3522c17c69395aead736b0edbdc5278d6212c779
jlangdon11 <JordanLangdon11@gmail.com>
2026-09-02 12:17:53 -0400

    Add files via upload

---
+++ ReturnComputerOU.ps1
@@ -0,0 +1,9 @@
+﻿# Tries to find a match with the name like the DT or LT number that was entered
+
+while ($true) {
+
+$Adcomputer = Read-Host "Enter a DT or LT number"
+
+(Get-ADComputer -Filter "Name -like '*$Adcomputer*'" -Properties CanonicalName).CanonicalName
+
+}
