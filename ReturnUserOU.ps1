c31efff867781d9529b1781d59e8a47929e51ddc
jlangdon11 <JordanLangdon11@gmail.com>
2026-09-02 12:18:20 -0400

    Add files via upload

---
+++ ReturnUserOU.ps1
@@ -0,0 +1,9 @@
+﻿# Finds user by Canonical Name aka their logon username
+
+while ($true) {
+
+$Aduser = Read-Host "Enter the username"
+
+(Get-ADUser -Filter "SamAccountName -like '*$Aduser*'" -Properties CanonicalName).CanonicalName 
+
+}
